e4af679f78674cd4af7f996112ddf807b6db88c2
Malgorzata Matyjanka <m.matyjanka@gmail.com>
2019-04-23 21:44:27 +0100

    Add side div

--- script.js
+++ script.js
@@ -2,37 +2,52 @@ var prefix = 'https://cors-anywhere.herokuapp.com/';
 var url = 'https://swapi.co/api/people';
 var starWarsCharacters = document.getElementById('sWC');
 
+var section = document.getElementById('section');
+var div = document.createElement('div');
+section.insertAdjacentElement('beforeend', div);
+
 
 document.addEventListener('DOMContentLoaded', (e) => {
     showSWCharacters();
 
 });
 
-
-
 function showSWCharacters() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', prefix + 'https://swapi.co/api/people');
-    xhr.addEventListener('load', function () {
-        document.getElementById("spinner").remove();
+    get(prefix + url, renderCharacters);
 
-        var response = JSON.parse(xhr.response);
-        console.log(response.results)
-        starWarsCharacters.innerHtml = '';
-
-        for (var i = 0; i < response.results.length; i++) {
-            var liEl = document.createElement('li');
-            liEl.innerText = response.results[i].name;
-            starWarsCharacters.append(liEl);
-            id = i;
-            liEl.addEventListener('click', function () {
-                var div = document.createElement('div');
-                div.innerText = response.results[id].height;
-                liEl.append(div);
-            })
-        }
-
-
-    });
-    xhr.send();
 };
+
+function get(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', callback);
+    xhr.send();
+}
+
+function renderCharacters(e) {
+    var response = JSON.parse(event.target.response);
+
+    var html = response.results.map((person) => {
+        return '<li data-url="' + person.url + '">' + person.name + '</li>';
+    }).join('');
+
+    document.getElementById("spinner").remove();
+
+    starWarsCharacters.innerHTML = html;
+
+    starWarsCharacters.addEventListener('click', function (e) {
+        if (!event.target.matches('li'));
+        get(event.target.dataset.url, showDetails)
+    });
+
+}
+
+function showDetails() {
+    var response = JSON.parse(event.target.response);
+    div.innerHTML = "";
+    div.insertAdjacentHTML('afterbegin', `Gender: ${response.gender} <br>`);
+    div.insertAdjacentHTML('afterbegin', `Eye color: ${response.eye_color} <br>`);
+    div.insertAdjacentHTML('afterbegin', `Hair color: ${response.hair_color} <br>`);
+    div.insertAdjacentHTML('afterbegin', `Height: ${response.height} <br>`);
+    div.insertAdjacentHTML('afterbegin', `Name: ${response.name} <br>`);
+}
